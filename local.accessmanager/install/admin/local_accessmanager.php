a686a05251e3675b264ee0e1f29c6c402333998c
PimlikoFluffyButtons <128000957+PimlikoFluffyButtons@users.noreply.github.com>
2026-01-19 02:40:44 +0300

    Add files via upload

--- local.accessmanager/install/admin/local_accessmanager.php
+++ local.accessmanager/install/admin/local_accessmanager.php
@@ -4,6 +4,27 @@
  * Этот файл копируется в /bitrix/admin/ при установке модуля
  */
 
+// Проверяем AJAX ДО подключения пролога
+$isAjax = (
+    isset($_SERVER['HTTP_X_REQUESTED_WITH']) && 
+    strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest'
+) || (
+    isset($_POST['action']) && !empty($_POST['action'])
+);
+
+if ($isAjax) {
+    // Минимальная инициализация для AJAX (без HTML)
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php';
+    
+    // Подключаем модуль
+    \Bitrix\Main\Loader::includeModule('local.accessmanager');
+    
+    // Подключаем AJAX-обработчик
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/local/modules/local.accessmanager/admin/accessmanager_ajax.php';
+    die();
+}
+
+// Обычная загрузка страницы с HTML
 require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_admin_before.php';
 
 // Подключаем модуль
